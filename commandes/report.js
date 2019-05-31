12b7572614bd95729791a23d2e7f09855c358dfe
Mrgoldoff <50745100+Mrgoldoff@users.noreply.github.com>
2019-06-01 00:02:56 +0200

    Update report.js

--- commandes/report.js
+++ commandes/report.js
@@ -1,28 +1,27 @@
-const  Discord  =  require ( " discord.js " );
+const Discord = require("discord.js");
 
-module . exportations . run  =  async ( bot , message , args ) => {
-    laisser rUtilisateur =  message . guilde . membre ( message . mentions . utilisateurs . first () ||  message . guilde . membres . get (args [ 0 ]));
-    if ( ! rUser) renvoie un  message . canal . send ( " Impossible de trouver l'utilisateur. " );
-    laissez rreason =  args . rejoindre ( "  " ). tranche ( 22 );
+module.exports.run = async (bot, message, args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Impossible de trouvez l'utilisateur.");
+    let rreason = args.join(" ").slice(22);
 
-    let reportEmbed =  new  Discord.RichEmbed ()
-    . setDescription ( " Reports " )
-    . setColor ( " # 15f153 " )
-    . addField ( " Utilisateur signalé " , ` $ { rUser } avec l'ID: $ { rUser . id } ` )
-    . addField ( " Rapporté par " , ` $ { message . author } avec l'ID: $ { message . author . id } ` )
-    . addField ( " Channel " , message . channel )
-    . addField ( " Time " , message . createdAt )
-    . addField ( " Reason " , rreason);
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#15f153")
+    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
 
-    let reportschannel =  message . guilde . canaux . trouver ( ` nom ` , " rapports " );
-    if ( ! reportschannel) renvoie un  message . canal . send ( " Impossible de trouver le canal des rapports. " );
+    
 
 
-    message . delete (). catch ( O_o => {});
-    rapportschannel . envoyer (rapportEmbed);
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
 
 }
  
-module . exportations . help  = {
-  nom :  " rapport "
+module.exports.help = {
+  name: "report"
 }
